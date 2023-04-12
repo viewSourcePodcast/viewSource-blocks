2bcb5c7ecede9399a3320b1b101401fe5df92aa5
Aurooba Ahmed <hey@aurooba.com>
2023-04-12 07:59:48 -0600

    apply stash

--- functions.php
+++ functions.php
@@ -7,6 +7,8 @@
  */
 function viewsource_acf_enqueue_styles() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array(), '1.0' );
+	// enqueue child theme style
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ), '1.0' );
 }
 
 add_action( 'wp_enqueue_scripts', 'viewsource_acf_enqueue_styles' );
@@ -23,12 +25,82 @@ function register_acf_blocks() {
 add_action( 'init', 'register_acf_blocks' );
 
 /**
- * Register native blocks
+ * Enqueue a script to the frontend of WordPress
  *
  * @return void
  */
-function register_native_blocks() {
-	register_block_type( __DIR__ . '/build/hello-world' );
-	register_block_type( __DIR__ . '/build/custom-hello-world' );
+function vs_add_accordion_scripts() {
+
+	$asset_file = include get_stylesheet_directory() . '/build/accordion/index.asset.php';
+	$dependencies = $asset_file['dependencies'];
+	wp_enqueue_script(
+		'vs-accordion-script',
+		get_stylesheet_directory_uri() . '/build/accordion/index.js',
+		$dependencies,
+		$asset_file['version'],
+		true,
+	);
+
+	wp_enqueue_style( 'vs-accordion-styles', get_stylesheet_directory_uri() . '/build/accordion/style-index.css', false, $asset_file['version'] );
+
 }
-add_action( 'init', 'register_native_blocks' );
+add_action( 'wp_enqueue_scripts', 'vs_add_accordion_scripts' );
+
+
+// add an acf options page called Custom Date inside the Posts menu on init
+function vs_custom_date_options_page() {
+	if ( function_exists( 'acf_add_options_page' ) ) {
+		acf_add_options_page(
+			array(
+				'page_title' => 'Custom Date',
+				'menu_title' => 'Custom Date',
+				'menu_slug'  => 'custom-date',
+				'capability' => 'edit_posts',
+				'parent_slug' => 'edit.php',
+				'position'   => 20,
+				'icon_url'   => 'dashicons-calendar-alt',
+				'redirect'   => false,
+			)
+		);
+	}
+}
+add_action( 'init', 'vs_custom_date_options_page' );
+
+
+
+/**
+ * Add a custom date to the content of the post
+ *
+ * @param string $content
+ * @return string $content
+ */
+function vs_custom_date_info ( $content ) {
+	if ( is_single() ) {
+		$post_id = get_the_ID();
+		// get the date from the post
+		$post_date = get_the_date( 'F j, Y', $post_id );
+		// get the modified date from the post
+		$post_modified_date = get_the_modified_date( 'F j, Y', $post_id );
+		// get the custom date options
+		$custom_date = get_field( 'date_labels', 'option' );
+
+		$date = '';
+
+		foreach ( $custom_date as $key => $value ) {
+			$day         = $value['day'];
+			$date_output = 'published' === $value['date_output'] ? $post_date : $post_modified_date;
+			$custom      = 'custom' === $date_output ? $value['custom'] : false;
+			if ( $day === date( 'N' ) && get_the_date( 'N', $post_id ) === date( 'N' ) ) {
+
+				$date = sprintf( '<p class="custom-date-section">%s</p>', $custom ? $custom : $date_output );
+			}
+		}
+
+		$content = $date . $content;
+
+
+	}
+
+	return $content;
+}
+add_filter( 'the_content', 'vs_custom_date_info' );
