7efcde69c20b5fe562aa40fb7282f2ca7c345d3d
Aurooba Ahmed <hey@aurooba.com>
2023-05-19 09:55:20 -0600

    fix placeholder centre issue

--- build/accordion-item-flexible-content/index.asset.php
+++ build/accordion-item-flexible-content/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'c98b259b9df3b9726a21');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b1c8ab567e1bb9bbeee2');
--- build/accordion-item-flexible/index.asset.php
+++ build/accordion-item-flexible/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'dd6028c4000808d466c9');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b34fd167ef70c23d29d9');
